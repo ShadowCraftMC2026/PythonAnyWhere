8e14730e14445685fee4e298f5b75482b050aa01
ShadowCraftMC <mastergarik126@gmail.com>
2026-05-20 02:50:26 +0400

    Update start.sh to clone repo and install packages
    Clone the repository and install dependencies before running the app.

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd PythonAnyWhere
+git clone https://github.com/ShadowCraftMC2026/PythonAnyWhere
+cd /root/ShadowCraft2026/PythonAnyWhere
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3
+sudo apt install pip3
 pip3 install -r requirements.txt
 python3 app.py
