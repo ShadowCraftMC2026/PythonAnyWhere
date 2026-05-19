c6c6467eacac648ccdcfb568cd12eb2840ca339a
ShadowCraftMC <mastergarik126@gmail.com>
2026-05-20 02:50:55 +0400

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/ShadowCraftMC2026/PythonAnyWhere
-cd /root/ShadowCraft2026/PythonAnyWhere
-sudo apt update && sudo apt upgrade -y
-sudo apt install python3
-sudo apt install pip3
-pip3 install -r requirements.txt
-python3 app.py
